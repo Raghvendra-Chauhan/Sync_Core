f0e5d6b291746984860aab808686fa38b0d48e4e
Samarth <151175065+Samarth-3910@users.noreply.github.com>
2026-04-07 15:50:00 +0530

    test
    Remove sensitive MongoDB connection string.

--- test-mongo.js
+++ test-mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = "mongodb://samarthkeshari8_db_user:s5sghJu9hvfXXZn2@ac-5ntpqe4-shard-00-00.jayytfd.mongodb.net:27017,ac-5ntpqe4-shard-00-01.jayytfd.mongodb.net:27017,ac-5ntpqe4-shard-00-02.jayytfd.mongodb.net:27017/?ssl=true&replicaSet=atlas-glt3kv-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
+const uri = "";
 
 async function testMongo() {
     try {
